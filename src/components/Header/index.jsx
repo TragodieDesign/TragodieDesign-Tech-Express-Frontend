3c950675807eab2bdc96bd3e2987844755d45dc1
TragodieDesign <brunofernando@tragodiedesign.com.br>
2023-04-25 01:47:17 -0300

    Started header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,51 @@
-const Header = () =>{
-    return <div>Header</div>
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #353535;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  position: fixed;
+  width:100%;
+  font-family: 'Montserrat', sans-serif;
+  color: white;
+`
+
+const Logo = styled.img`
+  height: 60px;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1rem;
+`
+
+const Link = styled.a`
+  font-size: 1.2rem;
+  color: #white;
+  text-decoration: none;
+  &:hover {
+    color: #666666;
+  }
+`
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Logo src="/Site-e-commerce/img/logo/4x/Logo TEch Express.png" alt="TechExpress" />
+      <Nav>
+      <Link href="/Site-e-commerce/index.html">Home</Link>
+        <Link href="/Site-e-commerce/Categorias/celulares.html">Celulares</Link>
+        <Link href="/Site-e-commerce/Categorias/eletronicos.html">Eletrônicos</Link>
+        <Link href="/Site-e-commerce/Categorias/informatica.html">Informática</Link>
+        <Link href="/Site-e-commerce/Carrinho/index.html">Carrinho</Link>
+        <Link href="/Site-e-commerce/Finalizar/index.html">Finalizar Compra</Link>
+        <Link href="/Site-e-commerce/Sobre/index.html">Sobre nós</Link>
+      </Nav>
+    </HeaderContainer>
+  )
 }
-export default Header
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import { Provider } from 'react-redux'
+import { store } from '@/store/store'
 import { GlobalStyle } from '../styles/global-styles'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
@@ -6,11 +8,13 @@ import {Header} from '../components/All'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+            <ThemeProvider theme={theme}>
     <GlobalStyle/>
     <Header/>
   <Component {...pageProps} />
   </ThemeProvider>
+      </Provider>
   )
 
 }
