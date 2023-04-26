edeb81cb6c82e728199edfe3a4d83874e3cb72d7
TragodieDesign <brunofernando@tragodiedesign.com.br>
2023-04-25 23:56:19 -0300

    Investigating error from Copyright

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, CopyRight } from 'react-bootstrap';
 
 const Footer = () => {
   return (
